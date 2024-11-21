5c89d1ec1295d3a97cd3bf4fe989d4e2556151fd
studiosatswarit <swaritme9@outlook.com>
2024-11-21 17:39:24 +0530

    Update main.js

--- main.js
+++ main.js
@@ -17,7 +17,7 @@
 
 	async function execute(url, oldScript) {
 		if (oldScript) oldScript.type = 'javascript/blocked';
-		let data = await fetch("https://raw.githubusercontent.com/7GrandDadPGN/VapeForMiniblox/main/injection.js").then(e => e.text());
+		let data = await fetch("https://raw.githubusercontent.com/studiosatswarit/VapeForMiniblox/refs/heads/main/injection.js").then(e => e.text());
 		if (oldScript) oldScript.type = 'module';
 		eval(data.replace("scripturl", url));
 	}
